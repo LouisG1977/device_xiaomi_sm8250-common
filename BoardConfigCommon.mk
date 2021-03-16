3dd7788f2ab812ee845ecfa08b51949a1486f9ba
Pig <pig.priv@gmail.com>
2021-03-16 19:57:23 +0300

    sm8250-common: Move DPM feature property to /system_ext
    * DPM blobs are under /system_ext.
    
    Signed-off-by: Sanjivns <113604406+Sanjivns@users.noreply.github.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,6 +180,7 @@ TARGET_VENDOR_PROP += $(COMMON_PATH)/properties/vendor.prop
 ifneq ($(TARGET_IS_TABLET),true)
 TARGET_VENDOR_PROP += $(COMMON_PATH)/properties/vendor_phone.prop
 endif
+TARGET_SYSTEM_EXT_PROP += $(COMMON_PATH)/system_ext.prop
 
 # Recovery
 ifeq ($(TARGET_IS_VAB),true)
